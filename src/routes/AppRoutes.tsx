a5c6ddf62087dc1ed36979fa311d155758e30354
bibekoli <git@bibekoli.com>
2025-03-01 19:26:43 +0545

    refactor routing structure to include DashboardLayout and new pages

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,23 +1,39 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "@/pages/Landing";
 import NotFoundPage from "@/pages/NotFoundPage";
-import ChatPage from "@/pages/ChatPage";
-import LoginPage from "@/pages/Login";
-import RegisterPage from "@/pages/Register";
-import RouteProgress from "@/components/RouteProgress";
 import ProtectedRoute from "./ProtectedRoute";
+import DashboardLayout from "@/components/layout/DashboardLayout";
+import DashboardPage from "@/pages/DashboardPage";
+import ProfilePage from "@/pages/ProfilePage";
+import RouteProgress from "@/components/RouteProgress";
+import LandingPage from "@/pages/Landing";
+import LoginPage from "@/pages/Login";
+import SettingsPage from "@/pages/SettingsPage.index";
+import ChatPage from "@/pages/ChatPage";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-    <RouteProgress />
+      <RouteProgress />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+
         <Route element={<ProtectedRoute />}>
-          <Route path="/chat" element={<ChatPage />} />
+          <Route
+            path="/*"
+            element={
+              <DashboardLayout>
+                <Routes>
+                  <Route path="/dashboard" element={<DashboardPage />} />
+                  <Route path="/chats" element={<ChatPage />} />
+                  <Route path="/profile" element={<ProfilePage />} />
+                  <Route path="/settings" element={<SettingsPage />} />
+                </Routes>
+              </DashboardLayout>
+            }
+          />
         </Route>
+
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
