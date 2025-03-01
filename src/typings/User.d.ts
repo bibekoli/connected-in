b4bde97bd40a4d048fec7314b94f1020aeebc78b
bibekoli <git@bibekoli.com>
2025-03-01 14:10:45 +0545

    add User interface typings for user data structure

---
+++ src/typings/User.d.ts
@@ -0,0 +1,11 @@
+interface User {
+  user_id: string;
+  name: string;
+  email: string;
+  phone: string;
+  password?: string;
+  avatar: string;
+  address: string;
+  created_at: string;
+  last_online: string;
+}
